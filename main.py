06b0ed9fc8492b68d8d949d20c94f8fd6c2eecd2
Carlos Niquini <niquini.carlos@gmail.com>
2018-06-01 22:02:27 +0100

    Add new functions
    - functions to plot each info
    - functions to export and import all data into a json file

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import argparse
 import re
 import operator
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib.ticker import FuncFormatter
 import numpy as np
+import json
 
 """
 _dict_=	{'users':{'name|number':{'n_msg':0, 'n_words':0, 'n_charac':0, 'n_by_time': { '00':0, '01':0, ...,'23':0}}}
@@ -17,7 +19,33 @@ args = vars(ap.parse_args())
 
 u = 0
 
-def graf_v(l, n):
+def plot_msgs_by_users(_d_, _u_):
+	users_n_msg = sorted([(item[0], item[1]["n_msg"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
+	graf_h(users_n_msg, _u_, "Mensagens enviadas por usuario")
+
+def plot_words_by_users(_d_, _u_):
+	users_n_words = sorted([(item[0], item[1]["n_words"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
+	graf_h(users_n_words, _u_, "Palavras enviadas por usuario")
+
+def plot_charac_by_users(_d_, _u_):
+	users_n_charac = sorted([(item[0], item[1]["n_charac"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
+	graf_h(users_n_charac, _u_, "Caracteres enviados por usuario")
+	
+def plot_chat_msgs_by_time(_d_):
+	chat_n_by_time = sorted([(item[0], item[1]) for item in _dict_['_chat_info_']['n_by_time'].items()])
+	graf_v(chat_n_by_time, len(chat_n_by_time), "Total de mensagens enviadas por hora")
+
+def export(_d_):
+	with open('data.json', 'w') as fp:
+		json.dump(_d_, fp)
+
+def import_data():
+	file = open('data.json')
+	_str_ = file.read()
+	data = json.loads(_str_)
+	return data
+
+def graf_v(l, n, t):
 	x = np.arange(n)
 	y = []
 	label = []
@@ -25,11 +53,14 @@ def graf_v(l, n):
 		label.append(l[i][0])
 		y.append(l[i][1])
 	fig, ax = plt.subplots()
-	plt.bar(x, y)
+	ax.set_title(t)
+	ax.set_xlabel('Hora')
+	#plt.bar(x, y, color=(0, 0, 1))
+	plt.bar(x, y, color=(0, 0, 1))
 	plt.xticks(x, label)
 	plt.show()
 
-def graf_h(l, n):
+def graf_h(l, n, t):
 	x = np.arange(n)
 	y = []
 	label = []
@@ -37,6 +68,7 @@ def graf_h(l, n):
 		label.append(l[i][0])
 		y.append(l[i][1])
 	fig, ax = plt.subplots()
+	ax.set_title(t)
 	ax.barh(x, y)
 	ax.set_yticks(x)
 	ax.set_yticklabels(label)
@@ -83,11 +115,10 @@ if args['users'] == None:
 else:
 	u = int(args['users']) if int(args['users'])<=len(_dict_['users']) else len(_dict_['users'])
 
+#import_data()
+#export(_dict_)	
+plot_msgs_by_users(_dict_, u)
+plot_words_by_users(_dict_, u)
+plot_charac_by_users(_dict_, u)
+plot_chat_msgs_by_time(_dict_)
 #sorted = sorted(_dict_['users'].items(), key=operator.itemgetter(1), reverse=True)
-users_n_msg = sorted([(item[0], item[1]["n_msg"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
-users_n_words = sorted([(item[0], item[1]["n_words"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
-users_n_charac = sorted([(item[0], item[1]["n_charac"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
-
-#chat_n_by_time = sorted([(item[0], item[1]) for item in _dict_['_chat_info_']['n_by_time'].items()])
-#graf_v(chat_n_by_time, len(chat_n_by_time))
-graf_h(users_n_charac, u)
