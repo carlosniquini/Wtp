2288c241e2bfcae3c66a9069f8d16028f41b8395
Carlos Niquini <niquini.carlos@gmail.com>
2018-06-01 20:14:28 +0100

    Data structure update and new features
    - dictionary now saves the following information about users: total of msg, total of words, total of characters, number of msg in each hour
    - the same information are saved about the chat
    - ordered arrays for each information
    - fixed #3

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import numpy as np
 
-_dict_ = {}
+"""
+_dict_=	{'users':{'name|number':{'n_msg':0, 'n_words':0, 'n_charac':0, 'n_by_time': { '00':0, '01':0, ...,'23':0}}}
+		'_chat_info_':{"n_msg":0, "n_words":0, "n_charac":0,"n_by_time":{'00':0, '01':0, ...,'23':0}}}
+"""
+_dict_ = {"users":{},"_chat_info_":{"n_msg":0, "n_words":0, "n_charac":0, "n_by_time":{}}}
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", required = True, help = "Path to the file")
 ap.add_argument("-u", "--users", required = False, help = "# of users to show in graph")
 args = vars(ap.parse_args())
 
-total_msgs = 0
 u = 0
 
 def graf_v(l, n):
@@ -39,28 +42,52 @@ def graf_h(l, n):
 	ax.set_yticklabels(label)
 	ax.invert_yaxis()
 	ax.set_xlabel('# Mensagens')
-	ax.annotate("# total de mensagens: %d\n# participantes: %d"%(total_msgs, len(_dict_)),
+	ax.annotate("# total de mensagens: %d\n# total de palavras: %d\n# total de caracteres: %d\n# participantes: %d"
+				 %(_dict_["_chat_info_"]["n_msg"],_dict_["_chat_info_"]["n_words"],_dict_["_chat_info_"]["n_charac"], len(_dict_['users'])),
 				xy=(0, 0), xycoords='data',
-                xytext=(y[-1]+100, n-1), textcoords='data'
+                xytext=(y[-1]+(y[0]-y[-1])/2, n-1), textcoords='data'
                 )
 	plt.show()
 	
 with open(args['file'], encoding="utf8") as file:
 	for line in file:
-		match = re.search(r'(?P<day>\d{1,2}\/\d{1,2}\/\d{2})\,\s(?P<hour>\d{2}:\d{2})\s-\s+(?:(?P<full_name>(?P<f_name>[A-Z][\w+]*)(\s*[A-Z][\w+]*)*)|.*(?P<number>(\+\d{1,3})\s(\d{1,4}\s\d{1,5}(\s|-)\d{1,4})))(:|.:)(?P<msg>.*)', line, re.UNICODE)
+		match = re.search(r'(?P<day>\d{1,2}\/\d{1,2}\/\d{2})\,\s(?P<hour>\d{2}:\d{2})\s-\s+(?:(?P<full_name>(?P<f_name>[A-Z][\w+]*)(\s*[A-Z][\w+]*)*)|.*(?P<number>(\+\d{1,3})\s(\d{1,4}\s\d{1,5}(\s|-)\d{1,4})))(:|.:)\s(\<Media.omitted\>)*(?P<msg>.*)', line, re.UNICODE)
 		if match:
-			total_msgs+=1
-			id = match.group('full_name') if match.group('full_name') else match.group('number')
-			if id not in _dict_:
-				_dict_[id] = 1
+			h, _ = match.group('hour').split(":")
+			# Informacoes gerais do grupo/chat
+			_dict_["_chat_info_"]["n_msg"]+=1
+			_dict_["_chat_info_"]["n_words"]+=len(match.group('msg').split())
+			_dict_["_chat_info_"]["n_charac"]+=len(match.group('msg'))
+			if h not in _dict_['_chat_info_']['n_by_time']:
+				_dict_['_chat_info_']["n_by_time"][h] = 1
 			else:
-				_dict_[id] += 1
+				_dict_['_chat_info_']["n_by_time"][h] += 1
 			
-if args['users'] == None:
-	u = len(_dict_)
-else:
-	u = int(args['users'])
-			
+			# Informacoes de usuario
+			id = match.group('full_name') if match.group('full_name') else (match.group('number').replace(" ", "")).replace("-", "")
+			if id not in _dict_['users']:
+				#_dict_['users'][id] = 1
+				_dict_['users'][id] = {"n_msg": 1, "n_words": len(match.group('msg').split()), "n_charac": len(match.group('msg')),"n_by_time":{h:1}}
+			else:
+				#_dict_['users'][id] += 1
+				_dict_['users'][id]["n_msg"] += 1
+				_dict_['users'][id]["n_words"] += len(match.group('msg').split())
+				_dict_['users'][id]["n_charac"] += len(match.group('msg'))
+				if h not in _dict_['users'][id]['n_by_time']:
+					_dict_['users'][id]["n_by_time"][h] = 1
+				else:
+					_dict_['users'][id]["n_by_time"][h] += 1
 
-sorted = sorted(_dict_.items(), key=operator.itemgetter(1), reverse=True)
-graf_h(sorted, u)
+if args['users'] == None:
+	u = len(_dict_['users'])
+else:
+	u = int(args['users']) if int(args['users'])<=len(_dict_['users']) else len(_dict_['users'])
+
+#sorted = sorted(_dict_['users'].items(), key=operator.itemgetter(1), reverse=True)
+users_n_msg = sorted([(item[0], item[1]["n_msg"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
+users_n_words = sorted([(item[0], item[1]["n_words"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
+users_n_charac = sorted([(item[0], item[1]["n_charac"]) for item in _dict_['users'].items()],key=operator.itemgetter(1), reverse=True)
+
+#chat_n_by_time = sorted([(item[0], item[1]) for item in _dict_['_chat_info_']['n_by_time'].items()])
+#graf_v(chat_n_by_time, len(chat_n_by_time))
+graf_h(users_n_charac, u)
